63d63c83b49b3d76761ec7150853904cf77f9ae3
Kayla Firestack <kfirestack@mbta.com>
2024-02-19 14:50:31 -0500

    rename to gtfs-schedule

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-	description = "GTFS Types from GTFS Spec";
+	description = "GTFS Types from GTFS Schedule Specification Webpage";
 
 	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 	inputs.devshell.url = "github:numtide/devshell";
@@ -27,7 +27,7 @@
 			perSystem = { pkgs, lib, self', system, ... }: let
 				craneLib = crane.lib.${system};
 
-				# src = craneLib.cleanCargoSource (craneLib.path ./gtfs-static-types);
+				# src = craneLib.cleanCargoSource (craneLib.path ./gtfs-schedule-types);
 				src = let
 					# Only keeps markdown files
 					graphQLFilter = path: _type: builtins.match ".*/src/.*\.gql$" path != null;
@@ -39,18 +39,18 @@
 							craneLib.filterCargoSources
 						];
 					cleanedSource = lib.cleanSourceWith {
-						src = craneLib.path ./gtfs-static-types; # The original, unfiltered source
+						src = craneLib.path ./gtfs-schedule-types; # The original, unfiltered source
 						filter = qglOrCargo;
 					};
 				# in cleanedSource;
 				in pkgs.stdenvNoCC.mkDerivation {
-					pname = "gtfs-static-types";
+					pname = "gtfs-schedule-types";
 					version = "0.0.1";
 
 					src = cleanedSource.outPath;
 					buildPhase = lib.concatLines [
 						"set -x"
-						"cp ${self'.packages.gtfs-static-rs}/*.rs ./src/"
+						"cp ${self'.packages.gtfs-schedule-rs}/*.rs ./src/"
 						"set +x"
 					];
 					doCheck = false;
@@ -81,17 +81,17 @@
 
 				cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-				gtfs-static-types = craneLib.buildPackage (commonArgs // {
+				gtfs-schedule-types = craneLib.buildPackage (commonArgs // {
 					inherit cargoArtifacts;
 				});
 
 				linesFrom = lib.concatLines;
 			in {
-				packages.default = self'.packages.gtfs-static-types-doc;
+				packages.default = self'.packages.gtfs-schedule-types-doc;
 
-				packages.gtfs-static-types = gtfs-static-types;
+				packages.gtfs-schedule-types = gtfs-schedule-types;
 
-				packages.gtfs-static-types-doc = craneLib.cargoDoc (commonArgs // {
+				packages.gtfs-schedule-types-doc = craneLib.cargoDoc (commonArgs // {
 					inherit cargoArtifacts;
 				});
 
@@ -102,33 +102,33 @@
 					hash = "sha256-QGLOPY9CVbW6BjcjBt2UmMh5tOeeozwE92p+vnZoK1o=";
 				};
 
-				packages.gtfs-static-html = pkgs.fetchurl {
-					name = "gtfs-static.html";
+				packages.gtfs-schedule-html = pkgs.fetchurl {
+					name = "gtfs-schedule.html";
 					url = "https://gtfs.org/schedule/reference";
 					hash = "sha256-r5qin6byMXoC5KQk8sfSg/knQkFjmC8KaoUS1uTb3mo=";
 				};
 
-				packages.gtfs-static-xhtml = pkgs.runCommand "gtfs-static.xhtml" {
+				packages.gtfs-schedule-xhtml = pkgs.runCommand "gtfs-schedule.xhtml" {
 					buildInputs = [pkgs.html-tidy];
 				} (linesFrom [
-					"tidy -asxhtml --new-inline-tags c -o $out ${self'.packages.gtfs-static-html} || true"
+					"tidy -asxhtml --new-inline-tags c -o $out ${self'.packages.gtfs-schedule-html} || true"
 				]);
 
-				# packages.gtfs-static-xml = pkgs.runCommand "gtfs-static.xml" {
+				# packages.gtfs-schedule-xml = pkgs.runCommand "gtfs-schedule.xml" {
 				# 	buildInputs = [ pkgs.saxon-he ];
 				# } (linesFrom [
 				# 	"saxon-he -t \\"
-				# 		"-s:${self'.packages.gtfs-static-xhtml} \\"
-				# 		"-xsl:${./src/xsl/gtfs-static.xml.xsl} \\"
+				# 		"-s:${self'.packages.gtfs-schedule-xhtml} \\"
+				# 		"-xsl:${./src/xsl/gtfs-schedule.xml.xsl} \\"
 				# 		"-o:$out"
 				# ]);
 
-				packages.gtfs-static-xml = pkgs.stdenvNoCC.mkDerivation {
-					pname = "gtfs-static.xml";
+				packages.gtfs-schedule-xml = pkgs.stdenvNoCC.mkDerivation {
+					pname = "gtfs-schedule.xml";
 					version = "0.0.1";
 
 					src = lib.cleanSourceWith {
-						filter = (path: _: builtins.match ".*(txt-fns|functions|gtfs-static.xml)\.xsl" path != null);
+						filter = (path: _: builtins.match ".*(txt-fns|functions|gtfs-schedule.xml)\.xsl" path != null);
 						src = ./src/xsl;
 					};
 
@@ -139,8 +139,8 @@
 						# "mkdir $out"
 
 						"saxon-he -t \\"
-							"-s:${self'.packages.gtfs-static-xhtml} \\"
-							"-xsl:gtfs-static.xml.xsl \\"
+							"-s:${self'.packages.gtfs-schedule-xhtml} \\"
+							"-xsl:gtfs-schedule.xml.xsl \\"
 							"-o:$out"
 					];
 
@@ -151,8 +151,8 @@
 
 				};
 
-				packages.gtfs-static-rs = pkgs.stdenvNoCC.mkDerivation {
-					pname = "gtfs-static-rs";
+				packages.gtfs-schedule-rs = pkgs.stdenvNoCC.mkDerivation {
+					pname = "gtfs-schedule-rs";
 					version = "0.0.2";
 
 					src = ./src/xsl;
@@ -164,13 +164,13 @@
 						"mkdir $out"
 
 						"saxon-he -t \\"
-							"-s:${self'.packages.gtfs-static-xml} \\"
-							"-xsl:gtfs-static.rs.xsl"
+							"-s:${self'.packages.gtfs-schedule-xml} \\"
+							"-xsl:gtfs-schedule.rs.xsl"
 					];
 
 					installPhase = linesFrom [
-						"cp ./gtfs-static/* $out/"
-						"ln -s ${self'.packages.gtfs-static-xml} $out/gtfs-static.xml"
+						"cp ./gtfs-schedule/* $out/"
+						"ln -s ${self'.packages.gtfs-schedule-xml} $out/gtfs-schedule.xml"
 					];
 
 				};
@@ -195,8 +195,8 @@
 
 				checks = {
 					inherit (self'.packages)
-						gtfs-static-types
-						gtfs-static-types-doc;
+						gtfs-schedule-types
+						gtfs-schedule-types-doc;
 
 					inherit (self'.devShells)
 						default;
