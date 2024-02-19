94c12b386988dc33890580dc0ee806a0e5daef54
Kayla Firestack <kfirestack@mbta.com>
2024-02-19 16:23:27 -0500

    feat:cleanup: move gtfs-schedule xslt into own directory

--- scope.nix
+++ scope.nix
@@ -10,16 +10,18 @@ makeScope newScope (self: {
 
 	mbta-gtfs = self.callPackage ./mbta-gtfs.nix {};
 
-	gtfs-schedule-html = self.callPackage ./gtfs-schedule-html.nix {};
+	gtfs-schedule-html = self.callPackage ./gtfs-schedule-xslt/gtfs-schedule-html.nix {};
 
-	gtfs-schedule-xhtml = self.callPackage ./gtfs-schedule-xhtml.nix {};
+	gtfs-schedule-xhtml = self.callPackage ./gtfs-schedule-xslt/gtfs-schedule-xhtml.nix {};
 
-	gtfs-schedule-xml = self.callPackage ./gtfs-schedule-xml.nix {};
+	gtfs-schedule-xml = self.callPackage ./gtfs-schedule-xslt/gtfs-schedule-xml.nix {};
 
-	gtfs-schedule-generated-rs-src = self.callPackage ./gtfs-schedule-generated-rs-src.nix {};
+	gtfs-schedule-generated-rs-src = self.callPackage ./gtfs-schedule-xslt/gtfs-schedule-generated-rs-src.nix {};
 
 	gtfs-schedule-types-rs-common-args = {
-		src = self.callPackage ./gtfs-schedule-types/combined-srcs.nix {};
+		src = self.callPackage ./gtfs-schedule-types/combined-srcs.nix {
+			inherit craneLib;
+		};
 
 		strictDeps = true;
 		doCheck = false;
@@ -29,9 +31,15 @@ makeScope newScope (self: {
 		];
 
 	};
-	gtfs-schedule-types-rs = self.callPackage (./gtfs-schedule-types) {};
+	gtfs-schedule-types-rs = self.callPackage (./gtfs-schedule-types) {
+		inherit craneLib;
+	};
 
-	gtfs-schedule-types-rs-doc = self.callPackage (./gtfs-schedule-types/rustdoc.nix) {};
+	gtfs-schedule-types-rs-doc = self.callPackage (./gtfs-schedule-types/rustdoc.nix) {
+		inherit craneLib;
+	};
 
-	gtfs-schedule-types-rs-cargo-artifacts = self.callPackage ./gtfs-schedule-types/cargo-artifacts.nix {};
+	gtfs-schedule-types-rs-cargo-artifacts = self.callPackage ./gtfs-schedule-types/cargo-artifacts.nix {
+		inherit craneLib;
+	};
 })
