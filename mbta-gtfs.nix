591ea7738de83070195931228a39530233c86ac9
Kayla Firestack <kfirestack@mbta.com>
2024-04-05 22:59:29 -0400

    review! chore update mbta archive hash

--- mbta-gtfs.nix
+++ mbta-gtfs.nix
@@ -3,5 +3,5 @@
 	name = "MBTA GTFS Static";
 	url = "https://cdn.mbtace.com/archive/20240111.zip";
 	stripRoot = false;
-	hash = "sha256-QGLOPY9CVbW6BjcjBt2UmMh5tOeeozwE92p+vnZoK1o=";
+	hash = "sha256-Nvo740l+eo/vnHzZ0qOIJwsUGrp/4CPDxTpXhVlrLnI=";
 }
