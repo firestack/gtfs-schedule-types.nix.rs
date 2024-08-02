af1ac14e48cf73324dc72b25ea853337cf03c8ec
Kayla Firestack <kfirestack@mbta.com>
2024-08-01 23:15:36 -0400

    fix: remove `thiserror` reminant  from parse

--- gtfs-schedule-types/src/parse.rs
+++ gtfs-schedule-types/src/parse.rs
@@ -1,17 +1,9 @@
 use std::path::Path;
-use thiserror::Error;
 
-#[derive(Error, Debug)]
-pub(crate) enum ParseCsvError {
-	#[error("wtf")]
-	Csv(#[from] csv::Error)
-}
-
-pub(crate) fn parse_csv<RecordType>(filename: &Path) -> Result<Vec<RecordType>, ParseCsvError>
+pub(crate) fn parse_csv<RecordType>(filename: &Path) -> Result<Vec<RecordType>, csv::Error>
 where
 	RecordType: serde::de::DeserializeOwned,
 {
 	csv::Reader::from_path(filename)
 		.and_then(|mut reader| reader.deserialize().collect::<Result<Vec<RecordType>, _>>())
-		.map_err(|e| e.into())
 }
